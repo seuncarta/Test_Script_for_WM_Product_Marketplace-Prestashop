13616346f772bded6fba55a774844fec401028e3
seuncarta <seuncarta@gmail.com>
2023-02-21 15:57:25 +0100

    first commit for the project after setting up the project

---
+++ UsersTest/1LoginTest.js
@@ -0,0 +1,39 @@
+const { Builder, Browser, By, Key, until } = require('selenium-webdriver');
+const assert = require("assert");
+
+describe("Test for Login", function () {
+
+    it("Valid scenario", async function () {
+        // Set up the Selenium WebDriver instance
+        let driver = await new Builder().forBrowser(Browser.CHROME).build();
+
+        // Navigate to the log in page on the service marketplace
+        await driver.get('https://wealthmarketshop.com/index.php?controller=authentication&back=my-account');
+
+        // Fill in the login form
+        const emailInput = driver.findElement(By.name('email'));
+        emailInput.sendKeys('seunjr7@gmail.com');
+
+        const passwordInput = driver.findElement(By.name('password'));
+        passwordInput.sendKeys('economicedu156');
+
+        // Submit the login form
+        const loginButton = driver.findElement(By.id('submit-login'));
+        loginButton.click();
+
+        await driver.wait(until.urlIs('https://wealthmarketshop.com/index.php?controller=my-account'), 5000, 'Showld load dashboard page');
+
+        //close the browser
+        await driver.quit();
+    })
+
+    // it("inValid scenario", async function () {
+
+    // })
+
+    // it("inValid scenario", async function () {
+
+    // })
+
+})
+
---
+++ UsersTest/2ProductSearch.js
@@ -0,0 +1,20 @@
+const { Builder, Browser, By, Key, until } = require('selenium-webdriver');
+const assert = require("assert");
+
+describe("Product Search", function () {
+
+    it("Search for Product on the marketplace", async function () {
+
+
+    })
+
+    it("Enter have of the product name and hit the search button", async function () {
+
+    })
+
+    it("Search for product not on the marketplace", async function () {
+
+    })
+
+})
+
